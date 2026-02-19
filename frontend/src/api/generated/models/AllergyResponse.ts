27ae3f7d0e1f92dfa448b7a81cd2ce7d945b3947
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:07 +0300

    chore(frontend): add frontend/src/api/generated/models/AllergyResponse.ts

---
+++ frontend/src/api/generated/models/AllergyResponse.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type AllergyResponse = {
+    allergen: string;
+    allergy_type: string;
+    severity: string;
+    reaction?: (string | null);
+    diagnosed_date?: (string | null);
+    notes?: (string | null);
+    id: number;
+    patient_id: number;
+    created_at: string;
+};
+
