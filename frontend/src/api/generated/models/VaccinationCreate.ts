e5055828d9e94ab9a7caca278c892d42b1584ede
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:39:57 +0300

    chore(frontend): add frontend/src/api/generated/models/VaccinationCreate.ts

---
+++ frontend/src/api/generated/models/VaccinationCreate.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type VaccinationCreate = {
+    vaccine_name: string;
+    dose_number?: (number | null);
+    date_administered: string;
+    administered_by?: (string | null);
+    location?: (string | null);
+    lot_number?: (string | null);
+    expiration_date?: (string | null);
+    site?: (string | null);
+    reaction?: (string | null);
+    notes?: (string | null);
+};
+
