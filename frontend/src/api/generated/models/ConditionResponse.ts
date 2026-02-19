5fc7704d68473816161d6b6aeccca959d8932af6
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:21 +0300

    chore(frontend): add frontend/src/api/generated/models/ConditionResponse.ts

---
+++ frontend/src/api/generated/models/ConditionResponse.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ConditionResponse = {
+    condition_name: string;
+    icd_code?: (string | null);
+    diagnosed_date?: (string | null);
+    status?: string;
+    severity?: (string | null);
+    treating_physician?: (string | null);
+    notes?: (string | null);
+    id: number;
+    patient_id: number;
+    created_at: string;
+};
+
