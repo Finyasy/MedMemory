a9b14588a63b742f360b6239e03c98ba8832f75d
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:18 +0300

    chore(frontend): add frontend/src/api/generated/models/ConditionCreate.ts

---
+++ frontend/src/api/generated/models/ConditionCreate.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ConditionCreate = {
+    condition_name: string;
+    icd_code?: (string | null);
+    diagnosed_date?: (string | null);
+    status?: string;
+    severity?: (string | null);
+    treating_physician?: (string | null);
+    notes?: (string | null);
+};
+
