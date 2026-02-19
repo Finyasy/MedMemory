ce90d071e553e6d5b1cdea5cdfd7a05299654931
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:39:30 +0300

    chore(frontend): add frontend/src/api/generated/models/LifestyleResponse.ts

---
+++ frontend/src/api/generated/models/LifestyleResponse.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type LifestyleResponse = {
+    smoking_status?: (string | null);
+    smoking_frequency?: (string | null);
+    alcohol_use?: (string | null);
+    exercise_frequency?: (string | null);
+    diet_type?: (string | null);
+    sleep_hours?: (string | null);
+    occupation?: (string | null);
+    stress_level?: (string | null);
+    id: number;
+    patient_id: number;
+};
+
