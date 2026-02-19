c371eeaa46ce8679b728766b3a99d02189a6c31e
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:39:01 +0300

    chore(frontend): add frontend/src/api/generated/models/FamilyHistoryResponse.ts

---
+++ frontend/src/api/generated/models/FamilyHistoryResponse.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type FamilyHistoryResponse = {
+    relation: string;
+    condition: string;
+    age_of_onset?: (number | null);
+    is_deceased?: boolean;
+    notes?: (string | null);
+    id: number;
+    patient_id: number;
+};
+
