45c50cae0b3d48f0477d6ef25b486e3c87422dde
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:46 +0300

    chore(frontend): add frontend/src/api/generated/models/EmergencyContactUpdate.ts

---
+++ frontend/src/api/generated/models/EmergencyContactUpdate.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type EmergencyContactUpdate = {
+    name?: (string | null);
+    relationship?: (string | null);
+    phone?: (string | null);
+    email?: (string | null);
+    is_primary?: (boolean | null);
+};
+
