3a3edd941139dfdc3e2e353ecc78ea9699b14056
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:25 +0300

    chore(frontend): add frontend/src/api/generated/models/ConditionUpdate.ts

---
+++ frontend/src/api/generated/models/ConditionUpdate.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ConditionUpdate = {
+    condition_name?: (string | null);
+    icd_code?: (string | null);
+    diagnosed_date?: (string | null);
+    status?: (string | null);
+    severity?: (string | null);
+    treating_physician?: (string | null);
+    notes?: (string | null);
+};
+
