7f61e793dbd4409f4dc29cef05973f3285491ca6
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:39:23 +0300

    chore(frontend): add frontend/src/api/generated/models/InsuranceCreate.ts

---
+++ frontend/src/api/generated/models/InsuranceCreate.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type InsuranceCreate = {
+    provider_name: string;
+    policy_number?: (string | null);
+    group_number?: (string | null);
+    subscriber_name?: (string | null);
+    subscriber_dob?: (string | null);
+    relationship_to_subscriber?: (string | null);
+    effective_date?: (string | null);
+    expiration_date?: (string | null);
+    is_primary?: boolean;
+};
+
