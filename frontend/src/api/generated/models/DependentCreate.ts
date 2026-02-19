69d4dd1842ac87a80287c01318cb8fd0be4ffab4
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:28 +0300

    chore(frontend): add frontend/src/api/generated/models/DependentCreate.ts

---
+++ frontend/src/api/generated/models/DependentCreate.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type DependentCreate = {
+    first_name: string;
+    last_name: string;
+    date_of_birth: string;
+    sex?: (string | null);
+    blood_type?: (string | null);
+    relationship_type: string;
+};
+
