93f42f433a549d5d627f3efbb0b86e3d2c394d5a
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:39:16 +0300

    chore(frontend): add frontend/src/api/generated/models/GrowthMeasurementCreate.ts

---
+++ frontend/src/api/generated/models/GrowthMeasurementCreate.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type GrowthMeasurementCreate = {
+    measurement_date: string;
+    age_months?: (number | null);
+    height_cm?: (number | string | null);
+    weight_kg?: (number | string | null);
+    head_circumference_cm?: (number | string | null);
+    height_percentile?: (number | null);
+    weight_percentile?: (number | null);
+    bmi?: (number | string | null);
+    bmi_percentile?: (number | null);
+    notes?: (string | null);
+};
+
