fff0b8d93158396b71aca98c6c9d3bb8cc0301d9
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:39:42 +0300

    chore(frontend): add frontend/src/api/generated/models/ProviderCreate.ts

---
+++ frontend/src/api/generated/models/ProviderCreate.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ProviderCreate = {
+    provider_type: string;
+    specialty?: (string | null);
+    name: string;
+    clinic_name?: (string | null);
+    phone?: (string | null);
+    fax?: (string | null);
+    email?: (string | null);
+    address?: (string | null);
+    is_primary?: boolean;
+    notes?: (string | null);
+};
+
