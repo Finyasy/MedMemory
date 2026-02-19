d79057a335a3ebe5b198e751d1cd7d20e6dfed36
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:03 +0300

    chore(frontend): add frontend/src/api/generated/models/AllergyCreate.ts

---
+++ frontend/src/api/generated/models/AllergyCreate.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type AllergyCreate = {
+    allergen: string;
+    allergy_type: string;
+    severity: string;
+    reaction?: (string | null);
+    diagnosed_date?: (string | null);
+    notes?: (string | null);
+};
+
