cd62af5d25113d82b013058a974b94dc13bd4844
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:40:00 +0300

    chore(frontend): add frontend/src/api/generated/models/VaccinationResponse.ts

---
+++ frontend/src/api/generated/models/VaccinationResponse.ts
@@ -0,0 +1,19 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type VaccinationResponse = {
+    vaccine_name: string;
+    dose_number?: (number | null);
+    date_administered: string;
+    administered_by?: (string | null);
+    location?: (string | null);
+    lot_number?: (string | null);
+    expiration_date?: (string | null);
+    site?: (string | null);
+    reaction?: (string | null);
+    notes?: (string | null);
+    id: number;
+    patient_id: number;
+};
+
