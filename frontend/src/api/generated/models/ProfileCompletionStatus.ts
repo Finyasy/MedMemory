66699b3101d5fe41e98192e987845122af2e5035
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:39:38 +0300

    chore(frontend): add frontend/src/api/generated/models/ProfileCompletionStatus.ts

---
+++ frontend/src/api/generated/models/ProfileCompletionStatus.ts
@@ -0,0 +1,9 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ProfileCompletionStatus = {
+    overall_percentage: number;
+    sections: Record<string, Record<string, any>>;
+};
+
