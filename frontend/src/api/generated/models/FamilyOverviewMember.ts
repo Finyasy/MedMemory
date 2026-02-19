3c156932251e8797b87c85ed81b858dd616d7460
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:39:05 +0300

    chore(frontend): add frontend/src/api/generated/models/FamilyOverviewMember.ts

---
+++ frontend/src/api/generated/models/FamilyOverviewMember.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type FamilyOverviewMember = {
+    id: number;
+    full_name: string;
+    age: (number | null);
+    sex: (string | null);
+    blood_type: (string | null);
+    relationship_type: (string | null);
+    document_count?: number;
+    last_activity?: (string | null);
+    alerts?: Array<string>;
+};
+
