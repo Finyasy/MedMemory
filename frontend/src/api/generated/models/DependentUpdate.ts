c0993e3893d028e6a56f626f011d80ac2a16282c
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:35 +0300

    chore(frontend): add frontend/src/api/generated/models/DependentUpdate.ts

---
+++ frontend/src/api/generated/models/DependentUpdate.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type DependentUpdate = {
+    first_name?: (string | null);
+    last_name?: (string | null);
+    date_of_birth?: (string | null);
+    sex?: (string | null);
+    blood_type?: (string | null);
+};
+
