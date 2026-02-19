9ba156a245bf96325a88663ac4ad36d1706998c4
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:57 +0300

    chore(frontend): add frontend/src/api/generated/models/FamilyHistoryCreate.ts

---
+++ frontend/src/api/generated/models/FamilyHistoryCreate.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type FamilyHistoryCreate = {
+    relation: string;
+    condition: string;
+    age_of_onset?: (number | null);
+    is_deceased?: boolean;
+    notes?: (string | null);
+};
+
