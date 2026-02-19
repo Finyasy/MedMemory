b2ce20670b804793b600d414b2f6118b06d2d220
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:14 +0300

    chore(frontend): add frontend/src/api/generated/models/BasicProfileUpdate.ts

---
+++ frontend/src/api/generated/models/BasicProfileUpdate.ts
@@ -0,0 +1,20 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type BasicProfileUpdate = {
+    first_name?: (string | null);
+    last_name?: (string | null);
+    date_of_birth?: (string | null);
+    sex?: (string | null);
+    gender?: (string | null);
+    blood_type?: (string | null);
+    height_cm?: (number | string | null);
+    weight_kg?: (number | string | null);
+    phone?: (string | null);
+    email?: (string | null);
+    address?: (string | null);
+    preferred_language?: (string | null);
+    timezone?: (string | null);
+};
+
