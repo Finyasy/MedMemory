3d3462502d07454dce924750015db2552403f523
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:50 +0300

    chore(frontend): add frontend/src/api/generated/models/EmergencyInfoResponse.ts

---
+++ frontend/src/api/generated/models/EmergencyInfoResponse.ts
@@ -0,0 +1,13 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type EmergencyInfoResponse = {
+    medical_alert?: (string | null);
+    organ_donor?: (boolean | null);
+    dnr_status?: (boolean | null);
+    preferred_hospital?: (string | null);
+    id: number;
+    patient_id: number;
+};
+
