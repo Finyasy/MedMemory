f5c67d875dba36cddb74b6951ee0da9c4e935340
Bryan Bosire <bryan.bosire@KEMDGF2B02.local>
2026-02-19 18:38:32 +0300

    chore(frontend): add frontend/src/api/generated/models/DependentResponse.ts

---
+++ frontend/src/api/generated/models/DependentResponse.ts
@@ -0,0 +1,18 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type DependentResponse = {
+    id: number;
+    first_name: string;
+    last_name: string;
+    full_name: string;
+    date_of_birth: (string | null);
+    age: (number | null);
+    sex: (string | null);
+    blood_type: (string | null);
+    relationship_type: string;
+    is_primary_caretaker: boolean;
+    can_edit: boolean;
+};
+
